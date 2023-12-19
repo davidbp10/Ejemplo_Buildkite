e0c89441036b93ea7bb8348e26ba498e9036bfd8
davidbp10 <david.breapinero@alum.uca.es>
2023-12-19 19:54:39 +0100

    cambios

--- scripts/test.sh
+++ scripts/test.sh
@@ -17,5 +17,6 @@ echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
-npm test --watchAll=false
+jest --watchAll=false
+npm test
 echo "Tests completed"'
